de6a92a9e689b1254ef2539caf1bc188ec5dbcb9
akithas <alex.kithas@aa.com>
2021-07-08 17:06:51 -0500

    Add basic gradle set up

---
+++ src/test/java/check/CheckTest.java
@@ -0,0 +1,13 @@
+package check;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CheckTest {
+
+    @Test
+    void testsRun() {
+        assertTrue(true);
+    }
+}
